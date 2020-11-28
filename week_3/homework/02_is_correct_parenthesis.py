e857df275545984aa1f0aa2a89e6f853ee860644
takoyummy <takoyummy@naver.com>
2020-11-28 22:28:32 +0900

    algo_1128cm
    11월 28일자 알고리즘 공부

--- week_3/homework/02_is_correct_parenthesis.py
+++ week_3/homework/02_is_correct_parenthesis.py
@@ -3,7 +3,19 @@ s = "(())()"
 
 def is_correct_parenthesis(string):
     # 구현해보세요!
-    return
+    r =0
+    if string[0] == ")" or None:
+        return False
+
+    for i in string:
+        if i =="(":
+            r += 1
+        elif i == ")":
+            r -= 1
+    if r == 0:
+        return True
+    else:
+        return False
 
 
 print(is_correct_parenthesis(s))  # True 를 반환해야 합니다!
--- week_3/homework/03_get_melon_best_album.py
+++ week_3/homework/03_get_melon_best_album.py
@@ -4,7 +4,28 @@ plays = [500, 600, 150, 800, 2500]
 
 def get_melon_best_album(genre_array, play_array):
     # 구현해보세요!
-    return []
+    result = []
+    total_dict = {}
+    index_dict = {}
+    for i in range(len(genre_array)):
+        genre = genre_array[i]
+        play = play_array[i]
+        if genre not in total_dict:
+            total_dict[genre] = play
+            index_dict[genre] = [[i, play]]
+        else:
+            total_dict[genre] += play
+            index_dict[genre].append([i, play])
+    total_dict2 = sorted(total_dict.items(), key=lambda item: item[1], reverse=True)
+    for genre, play in total_dict2:
+        index_array = index_dict[genre]
+        sorted_index_array = sorted(index_array, key=lambda item: item[1], reverse=True)
+        for i in range(len(sorted_index_array)):
+            if i >1 :
+                break
+            result.append(sorted_index_array[i][0])
+
+    return result
 
 
 print(get_melon_best_album(genres, plays))  # 결과로 [4, 1, 3, 0] 가 와야 합니다!
