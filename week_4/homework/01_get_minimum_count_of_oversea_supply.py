e183a552b4207c2d28957c8498f42914135da4fe
takoyummy <takoyummy@naver.com>
2020-12-07 20:59:46 +0900

    number1

--- week_4/homework/01_get_minimum_count_of_oversea_supply.py
+++ week_4/homework/01_get_minimum_count_of_oversea_supply.py
@@ -7,8 +7,23 @@ supply_recover_k = 30
 
 
 def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
+    answer = 0
+    current_day_index = 0
+    max_heap = []
     # 풀어보세요!
-    return
+    while stock < k:
+        for date_index in range(current_day_index, len(dates)):
+            print(date_index,dates[date_index],stock,supplies[date_index])
+            if dates[date_index] <= stock:
+                heapq.heappush(max_heap, -supplies[date_index])
+            else:
+                current_day_index = date_index
+                break
+        answer += 1
+        print("stock is", stock)
+        stock += -heapq.heappop(max_heap)
+    return answer
+
 
 
 print(get_minimum_count_of_overseas_supply(ramen_stock, supply_dates, supply_supplies, supply_recover_k))
